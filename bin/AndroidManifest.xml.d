d054a9ab91edf8eb23d971d1a49de9354d730c80
Kaito <kaaitokid@gmail.com>
2015-07-14 11:54:06 +0200

    updated and fixed movement

--- bin/AndroidManifest.xml.d
+++ bin/AndroidManifest.xml.d
@@ -1,2 +1,2 @@
-/home/VGASMB/kaito/vgafib-home/Git/GitInky/AndroidGravity/bin/AndroidManifest.xml : \
-/home/VGASMB/kaito/vgafib-home/Git/GitInky/AndroidGravity/AndroidManifest.xml \
+/home/kaito/Escriptori/GitProjects/AndroidGravity/bin/AndroidManifest.xml : \
+/home/kaito/Escriptori/GitProjects/AndroidGravity/AndroidManifest.xml \
